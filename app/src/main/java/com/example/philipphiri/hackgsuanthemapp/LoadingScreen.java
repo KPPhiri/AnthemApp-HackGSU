7638f56e9390d067f280eee7ae1cbdd840533ef1
bguerrero6 <bguerrero6@gatech.edu>
2018-03-24 16:57:11 -0400

    loading screen ui with placeholders for drawables

--- app/src/main/java/com/example/philipphiri/hackgsuanthemapp/LoadingScreen.java
+++ app/src/main/java/com/example/philipphiri/hackgsuanthemapp/LoadingScreen.java
@@ -4,12 +4,15 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class LoadingScreen extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        //setContentView(R.layout.activity_loading_screen);
 
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
